3c9d933ca59428cf6b3d6a827bc50989cf84959f
JBLew <autumnlewjb@gmail.com>
2020-12-07 22:14:27 +0800

    complete the design for homepage login and register

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
